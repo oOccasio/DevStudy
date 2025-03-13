268c1d6805240172cefa77bc62ba4423a09057fc
oOccasio <dhzktldh@gmail.com>
2025-03-13 15:37:35 +0900

    개발 시작

---
+++ minproject2 - board/chuchuchi/src/main/java/chuchuchi/chuchuchi/ChuchuchiApplication.java
@@ -0,0 +1,13 @@
+package chuchuchi.chuchuchi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuchuchiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChuchuchiApplication.class, args);
+	}
+
+}
---
+++ minproject2 - board/chuchuchi/src/test/java/chuchuchi/chuchuchi/ChuchuchiApplicationTests.java
@@ -0,0 +1,13 @@
+package chuchuchi.chuchuchi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuchuchiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
