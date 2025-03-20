673821c9feefb0a22479a3a232c63c62e45d86af
oOccasio <dhzktldh@gmail.com>
2025-03-20 22:04:50 +0900

    게시판 START

--- 미니프로젝트/workingservice/miniproject/src/main/java/chuchu/miniproject/domain/Role.java
+++
@@ -1,5 +0,0 @@
-package chuchu.miniproject.domain;
-
-public enum Role {
-    MANAGER, WORKER
-}
--- 미니프로젝트/workingservice/miniproject/src/main/java/chuchu/miniproject/dto/worker/request/RequestLeaveWorker.java
+++
@@ -1,9 +0,0 @@
-package chuchu.miniproject.dto.worker.request;
-
-import java.time.LocalDate;
-import java.time.LocalTime;
-
-public record RequestLeaveWorker(Long workerId,
-                                 LocalDate workDate,
-                                 LocalTime endTime) {
-}
