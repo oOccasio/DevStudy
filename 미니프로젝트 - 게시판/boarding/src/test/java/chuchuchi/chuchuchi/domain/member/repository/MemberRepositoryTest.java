71cbd6ea998feb5de6a18165f6517de927ec115e
oOccasio <dhzktldh@gmail.com>
2025-03-22 22:57:01 +0900

    test 코드 작성

--- 미니프로젝트 - 게시판/boarding/src/test/java/chuchuchi/chuchuchi/domain/member/repository/MemberRepositoryTest.java
+++ 미니프로젝트 - 게시판/boarding/src/test/java/chuchuchi/chuchuchi/domain/member/repository/MemberRepositoryTest.java
@@ -7,9 +7,12 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 
 @Transactional
 @SpringBootTest
@@ -62,7 +65,231 @@ public class MemberRepositoryTest {
                 .age(22)
                 .build();
 
+        //when, then
+        assertThrows(Exception.class, () -> memberRepository.save(member));
 
     }
+
+    @Test
+    public void user_save_no_name() throws Exception {
+
+        //given
+        Member member = Member.builder()
+                .username("username")
+                .password("1234567890")
+                .nickName("NickName1")
+                .role(Role.USER)
+                .age(22)
+                .build();
+
+        //when, then
+        assertThrows(Exception.class, () -> memberRepository.save(member));
+    }
+
+
+    @Test
+    public void user_save_no_nickName() throws Exception {
+
+        //given
+        Member member = Member.builder()
+                .username("username")
+                .password("1234567890")
+                .name("Member1")
+                .role(Role.USER)
+                .age(22)
+                .build();
+
+        //when, then
+        assertThrows(Exception.class, () -> memberRepository.save(member));
+
+    }
+
+    @Test
+    public void user_save_no_age() throws Exception {
+
+        //given
+        Member member = Member.builder()
+                .username("username")
+                .password("1234567890")
+                .name("Member1")
+                .nickName("NickName1")
+                .role(Role.USER)
+                .build();
+
+        //when, then
+        assertThrows(Exception.class, () -> memberRepository.save(member));
+    }
+
+    @Test
+    public void duplicate_user() throws Exception {
+
+        //given
+        Member member1 = Member.builder()
+                .username("username")
+                .password("1234567890")
+                .name("Member1")
+                .nickName("NickName1")
+                .role(Role.USER)
+                .age(22)
+                .build();
+
+        Member member2 = Member.builder()
+                .username("username")
+                .password("1234567890")
+                .name("Member1")
+                .nickName("NickName1")
+                .role(Role.USER)
+                .age(22)
+                .build();
+
+        memberRepository.save(member1);
+        em.clear();
+
+        //when,then
+        assertThrows(Exception.class, () -> memberRepository.save(member2));
+
+    }
+
+    @Test
+    public void modify_user() throws Exception {
+
+        //given
+        Member member1 = Member.builder()
+                .username("username")
+                .password("1234567890")
+                .name("Member1")
+                .nickName("NickName1")
+                .role(Role.USER)
+                .age(22)
+                .build();
+
+        memberRepository.save(member1);
+
+        String updatePassword = "updatePassword";
+        String updateName = "updateName";
+        String updateNickName = "updateNickName";
+        int updateAge = 33;
+
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+        em.clear();
+
+        //when
+        Member findMember = memberRepository.findById(member1.getId()).orElseThrow(Exception::new);
+        findMember.updateName(updateName);
+        findMember.updateNickName(updateNickName);
+        findMember.updateAge(updateAge);
+        findMember.updatePassword(passwordEncoder, updatePassword);
+        em.flush();
+
+        //then
+
+        Member findUpdateMember = memberRepository.findById(member1.getId()).orElseThrow(Exception::new);
+
+        assertThat(findUpdateMember).isSameAs(findMember);
+        assertThat(passwordEncoder.matches(updatePassword, findUpdateMember.getPassword())).isTrue();
+        assertThat(findUpdateMember.getName()).isEqualTo(updateName);
+        assertThat(findUpdateMember.getName()).isNotEqualTo(member1.getName());
+
+    }
+
+
+    @Test
+    public void existsByUsername() throws Exception {
+
+        //given
+        Member member1 = Member.builder()
+                .username("username")
+                .password("1234567890")
+                .name("Member1")
+                .nickName("NickName1")
+                .role(Role.USER)
+                .age(22)
+                .build();
+
+        memberRepository.save(member1);
+
+        //When, Then
+        assertThat(memberRepository.existsByUsername("username")).isTrue();
+
+    }
+
+    @Test
+    public void delete_user() throws Exception {
+
+        Member member1 = Member.builder()
+                .username("username")
+                .password("1234567890")
+                .name("Member1")
+                .nickName("NickName1")
+                .role(Role.USER)
+                .age(22)
+                .build();
+
+        memberRepository.save(member1);
+        em.clear();
+
+        //when
+        memberRepository.delete(member1);
+        em.flush();
+        em.clear();
+
+
+        //then
+        assertThrows(Exception.class, () -> memberRepository.findById(member1.getId()).orElseThrow(Exception::new));
+
+
+    }
+
+    @Test
+    public void findByUsername() throws Exception {
+
+        //given
+        String username = "username";
+        Member member1 = Member.builder()
+                .username(username)
+                .password("1234567890")
+                .name("Member1")
+                .nickName("NickName1")
+                .role(Role.USER)
+                .age(22)
+                .build();
+
+        memberRepository.save(member1);
+        em.clear();
+
+        //when, then
+
+        assertThat(memberRepository.findByUsername(username).get().getUsername()).isEqualTo(member1.getUsername());
+        assertThat(memberRepository.findByUsername(username).get().getName()).isEqualTo(member1.getName());
+        assertThat(memberRepository.findByUsername(username).get().getId()).isEqualTo(member1.getId());
+        assertThrows(Exception.class, () -> memberRepository.findByUsername(username+"123").orElseThrow(Exception::new));
+
+    }
+
+    @Test
+    public void saveUser_Report_Time(){
+
+        //given
+        Member member1 = Member.builder()
+                .username("username")
+                .password("1234567890")
+                .name("Member1")
+                .nickName("NickName1")
+                .role(Role.USER)
+                .age(22)
+                .build();
+        memberRepository.save(member1);
+        em.clear();
+
+        //when
+        Member findMember = memberRepository.findById(member1.getId())
+                .orElseThrow(() -> new RuntimeException("Member not found"));
+
+        //then
+        assertThat(findMember.getCreatedDate()).isNotNull();
+        assertThat(findMember.getLastModifiedDate()).isNotNull();
+    }
+
 
 }
